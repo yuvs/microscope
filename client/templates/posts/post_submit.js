520c0c0ce943768ba84ac11e772478fba64d9107
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-07 17:59:00 -0500

    added a submit post page and link in header

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,17 @@
+/**
+ * Created by yuvraj.sidhu on 3/7/15.
+ */
+
+Template.postSubmit.events({
+    'submit form': function(e){
+        e.preventDefault();
+
+        var post = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        };
+
+        post._id = Posts.insert(post);
+        Router.go('postPage', post);
+    }
+});
