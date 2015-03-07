8281b9a22e88cca5668b0cd8dd4f0713fda639ca
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-07 10:29:06 -0500

    wired collection into postsList template

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -2,20 +2,8 @@
  * Created by yuvraj.sidhu on 3/7/15.
  */
 
-var postsData = [
-    {
-        title: 'ESPN',
-        url: 'http://espn.com'
-    },
-    {
-        title: 'Meteor',
-        url: 'http://meteor.com'
-    },
-    {
-        title: 'The Meteor Book',
-        url: 'http://themeteorbook.com'
-    }
-];
 Template.postsList.helpers({
-    posts: postsData
+    posts: function(){
+        return Posts.find();
+    }
 });
