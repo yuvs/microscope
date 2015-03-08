dd1dd769262dd1f0c78df9df84a76fbe1db9225b
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-08 10:59:04 -0400

    added edit posts form

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,35 @@
+/**
+ * Created by yuvraj.sidhu on 3/8/15.
+ */
+
+Template.postEdit.events({
+    'submit form': function(e){
+        e.preventDefault();
+
+        var currentPostId = this._id;
+
+        var postProperties = {
+            url: $(e.target).find('[name=url]').val(),
+            title: $(e.target).find('[name=title]').val()
+        }
+
+        Posts.update(currentPostId, {$set: postProperties}, function(error){
+            if (error){
+                // display error to the user
+                alert(error.reason);
+            } else {
+                Router.go('postPage', {_id: currentPostId});
+            }
+        });
+    },
+
+    'click .delete': function(e){
+        e.preventDefault();
+
+        if (confirm("Delete this post?")){
+            var currentPostId = this._id;
+            Posts.remove(currentPostId);
+            Router.go('postsList');
+        }
+    }
+});
