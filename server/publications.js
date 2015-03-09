e87d9b1179b87e935883925b94124852dbce4be9
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-09 15:40:03 -0400

    simple pub / sub for comments

---
+++ server/publications.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yuvraj.sidhu on 3/7/15.
+ */
+
+Meteor.publish('posts', function(){
+    return Posts.find();
+});
+
+Meteor.publish('comments', function(postId){
+    check(postId, String);
+    return Comments.find({postId: postid});
+});
