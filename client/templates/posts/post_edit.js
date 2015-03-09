8cd13feaf9e2576b773198fe7d40d708f567ad2f
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-09 00:14:43 -0400

    validate post contents on update

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -2,6 +2,19 @@
  * Created by yuvraj.sidhu on 3/8/15.
  */
 
+Template.postEdit.created = function(){
+    Session.set('postEditErrors', {});
+}
+
+Template.postEdit.helpers({
+    errorMessage: function(field){
+        return Session.get('postEditErrors')[field];
+    },
+    errorClass: function(field){
+        return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+    }
+});
+
 Template.postEdit.events({
     'submit form': function(e){
         e.preventDefault();
@@ -12,6 +25,10 @@ Template.postEdit.events({
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         }
+
+        var errors = validatePost(postProperties);
+        if (errors.title || errors.url)
+            return Session.set('postEditErrors', errors);
 
         Posts.update(currentPostId, {$set: postProperties}, function(error){
             if (error){
