5f3c9e55d78e3a0c31c2820e2bb0cc41aa4901b9
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-10 16:04:27 -0400

    added basic notifications collection

--- server/publications.js
+++ server/publications.js
@@ -10,3 +10,8 @@ Meteor.publish('comments', function(postId){
     check(postId, String);
     return Comments.find({postId: postId});
 });
+
+
+Meteor.publish('notifications', function(){
+    return Notifications.find();
+});
