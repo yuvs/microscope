5bed5ecb3005f6bdf409a3bccf75f95cf5571014
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-11 13:21:44 -0400

    more animation - fade in and fade out

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -4,6 +4,12 @@
 
 Template.postsList.rendered = function(){
     this.find('.wrapper')._uihooks = {
+        insertElement: function(node, next){
+            $(node)
+                .hide()
+                .insertBefore(next)
+                .fadeIn();
+        },
         moveElement: function(node, next) {
             var $node = $(node), $next = $(next);
             var oldTop = $node.offset().top;
@@ -36,6 +42,11 @@ Template.postsList.rendered = function(){
             // reset everything to 0, animated
             $node.addClass('animate').css('top', 0);
             $inBetween.addClass('animate').css('top', 0);
+        },
+        removeElement: function(node){
+            $(node).fadeOut(function(){
+                $(this).remove();
+            });
         }
     }
 }
