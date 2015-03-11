d2199d38f4894ed430728db3d7818188944ff33a
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-11 09:11:10 -0400

    added active classes to the header

---
+++ client/templates/includes/header.js
@@ -0,0 +1,16 @@
+/**
+ * Created by yuvraj.sidhu on 3/11/15.
+ */
+
+Template.header.helpers({
+    activeRouteClass: function(/* route names */){
+        var args = Array.prototype.slice.call(arguments, 0);
+        args.pop();
+
+        var active = _.any(args, function(name){
+            return Router.current() && Router.current().route.getName() === name
+        });
+
+        return active && 'active';
+    }
+});
