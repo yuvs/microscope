07ffe082446962297a82abc23c74c4b6ef14655d
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-11 00:08:51 -0400

    added basic upvoting of posts

--- server/fixtures.js
+++ server/fixtures.js
@@ -23,7 +23,9 @@ if (Posts.find().count() === 0) {
         author: sacha.profile.name,
         url: 'http://sachagreif.com/introducing-telescope',
         submitted: new Date(now - 7 * 3600 * 1000),
-        commentsCount: 2
+        commentsCount: 2,
+        upvoters: [],
+        votes: 0
     });
 
     Comments.insert({
@@ -48,7 +50,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://meteor.com',
         submitted: new Date(now - 10 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     Posts.insert({
@@ -57,7 +61,9 @@ if (Posts.find().count() === 0) {
         author: tom.profile.name,
         url: 'http://themeteorbook.com',
         submitted: new Date(now - 12 * 3600 * 1000),
-        commentsCount: 0
+        commentsCount: 0,
+        upvoters: [],
+        votes: 0
     });
 
     for (var i = 0; i < 10; i++){
@@ -67,7 +73,9 @@ if (Posts.find().count() === 0) {
             userId: sacha._id,
             url: 'http://google.com/?q=test-' + i,
             submitted: new Date(now - i * 3600 * 1000),
-            commentsCount: 0
+            commentsCount: 0,
+            upvoters: [],
+            votes: 0
         });
     }
 }
