876c1da32c0fb356fd57e4b5e69b43fbc238be87
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-12 12:52:01 -0400

    shortening urls on post submit using bitly api - bug fixes

--- packages/bitly/bitly.js
+++ packages/bitly/bitly.js
@@ -4,25 +4,24 @@
 
 Bitly = {};
 
-Bitly.shortenedURL = function(url){
-    if (!Meteor.settings.bitly)
+Bitly.shortenURL = function(url){
+    if(!Meteor.settings.bitly)
         throw new Meteor.Error(500, 'Please provide a Bitly token in Meteor.settings');
 
     var shortenResponse = Meteor.http.get(
-        "https://api=ssl.bitly.com/v3/shorten?",
+        "https://api-ssl.bitly.com/v3/shorten?",
         {
             timeout: 5000,
-            params: {
+            params:{
                 "format": "json",
                 "access_token": Meteor.settings.bitly,
                 "longUrl": url
             }
         }
     );
-
-    if (shortenResponse.statusCode === 200){
+    if(shortenResponse.statusCode === 200){
         return shortenResponse.data.data.url
-    } else {
-        throw new Meteor.Error(500, "Bitly call failed with error: " + shortenResponse.status_txt);
+    }else{
+        throw new Meteor.Error(500, "Bitly call failed with error: "+shortenResponse.status_txt);
     }
 }
--- packages/bitly/package.js
+++ packages/bitly/package.js
@@ -8,7 +8,7 @@ Package.describe({
     version: '1.0.0'
 });
 
-Package.onUse(function(api){
+Package.onUse(function (api) {
     api.versionsFrom('0.9.4');
     api.addFiles('bitly.js', 'server');
     api.export('Bitly');
