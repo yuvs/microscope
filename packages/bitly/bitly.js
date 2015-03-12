41fdcd7740ace6703b9b1440d10afcaf6877301a
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-12 11:42:21 -0400

    added http package and created Bitly package

---
+++ packages/bitly/bitly.js
@@ -0,0 +1,3 @@
+/**
+ * Created by yuvraj.sidhu on 3/12/15.
+ */
---
+++ packages/bitly/package.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yuvraj.sidhu on 3/12/15.
+ */
+
+Package.describe({
+    name: 'bitly',
+    summary: "Bitly package",
+    version: '1.0.0'
+});
+
+Package.onUse(function(api){
+    api.versionsFrom('0.9.4');
+    api.addFiles('bitly.js', 'server');
+    api.export('Bitly');
+});
