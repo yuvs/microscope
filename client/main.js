7d10ffca67ec59bce10d49dd825f9c1b8ec7fec9
Yuvraj Sidhu <yuvsidhu@gmail.com>
2015-03-12 15:07:45 -0400

    added Intercom code snippet

--- client/main.js
+++ client/main.js
@@ -3,3 +3,13 @@
  */
 
 Meteor.subscribe('posts');
+
+window.intercomSettings = {
+    // TODO: The current logged in user's full name
+    name: "John Doe",
+    // TODO: The current logged in user's email address.
+    email: "john.doe@example.com",
+    // TODO: The current logged in user's sign-up date as a Unix timestamp.
+    created_at: 1234567890,
+    app_id: "kj7g27c0"
+};
---
+++ packages/intercom/package.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yuvraj.sidhu on 3/12/15.
+ */
+
+Package.describe({
+    name: 'intercom',
+    summary: "Intercom package",
+    version: '1.0.0'
+});
+
+Package.onUse(function(api){
+    api.versionsFrom('0.9.4');
+    api.addFiles('intercom_loader.js', 'client');
+});
